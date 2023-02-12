2707fccbb3eb5bc417cbd33289a8cf49fe5d9453
Smattacus <Smattacus@gmail.com>
2023-02-11 21:18:52 -0600

    Put up the container I want and a client to talk to it.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Hello world!
 This repository is for simple testing of containerizing a CICD flow of a MVC application with a client view into the code.
 
 
-Client <---RESTful http calls ----> Application <---- Primarily ORM based interactions with ----> Database (PG)
+Client <---RESTful (ideally) http calls ----> Application <---- Primarily ORM based interactions with ----> Database (PG)
 
---
+++ simple_client.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class EnvVars:
+    ENDPOINT = "APP_ENDPOINT"
+    PROTOCOL = "APP_PROTOCOL"
+
+class APIConfig:
+
+    def __init__(self):
+        self._endpoint = os.environ.get(EnvVars.ENDPOINT, "localhost:8000")
+        self._protocol = os.environ.get(EnvVars.PROTOCOL, "http")
+
+    def __str__(self):
+        return f"{self._protocol}://{self._endpoint}/"
+
+for i in range(5):
+    r = requests.get(f"{APIConfig()}")
+    logging.info(f"Request returned! Result: {r}")
